9bcbd2cee459d44ce20da21f8cacb9b906d65367
branhucks <branhucks@gmail.com>
2024-04-06 15:46:16 -0400

    Fixing HomeController class

--- project_gui/src/main/java/controllers/HomeController.java
+++ project_gui/src/main/java/controllers/HomeController.java
@@ -1,23 +1,49 @@
 package controllers;
 
-/**
- * Sample Skeleton for 'home.fxml' Controller Class
- */
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import model.*;
+import project_gui.App;
 
-public class HomeController {
+public class HomeController implements Initializable {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+    @FXML
+    private TextField txt_password;
+    @FXML
+    private TextField txt_username;
+    @FXML
+    private Label lbl_error;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+    @FXML
+    void btnSignInClicked(MouseEvent event) {
+        String username = txt_username.getText();
+        String password = txt_password.getText();
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+        FACADE facade = FACADE.getInstance();
+
+        if (!facade.login(username)) {
+            lbl_error.setText("Invalid login credentials");
+            return;
+        }
+
+        /**
+         * TODO
+         * App.setRoot("StudentLanding");
+         */
+    }
+
+    @FXML
+    void btnSignUpClicked(MouseEvent event) {
+        // App.setRoot("SignupStudent");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
 
     }
 
