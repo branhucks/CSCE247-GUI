eb0379d8509fb4dd7bc0010ea3d31d6186b7a5da
branhucks <branhucks@gmail.com>
2024-04-09 14:28:41 -0400

    Minor bug

--- project_gui/src/main/java/controllers/SignupController.java
+++ project_gui/src/main/java/controllers/SignupController.java
@@ -1,17 +1,17 @@
 package controllers;
 
-    import java.io.IOException;
-    import java.net.URL;
-    import java.util.ResourceBundle;
-    import javafx.fxml.FXML;
-    import javafx.fxml.Initializable;
-    import javafx.scene.control.Label;
-    import javafx.scene.control.TextField;
-    import javafx.scene.input.MouseEvent;
-    import model.*;
-    import project_gui.App;
- 
- public class SignupController implements Initializable {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import model.*;
+import project_gui.App;
+
+public class SignupController implements Initializable {
 
     @FXML
     private TextField stu_first_name;
@@ -42,50 +42,47 @@ package controllers;
     private Label adv_lbl_error;
 
     FACADE facade = FACADE.getInstance();
- 
-     @FXML // ResourceBundle that was given to the FXMLLoader
-     private ResourceBundle resources;
- 
-     @FXML // URL location of the FXML file that was given to the FXMLLoader
-     private URL location;
- 
-     @Override
-     public void initialize(URL url, ResourceBundle rb) {
 
-     }
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
 
-     @FXML
-     void btnStuSignup(MouseEvent event) throws IOException {
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    void btnStuSignup(MouseEvent event) throws IOException {
         String firstName = stu_first_name.getText();
         String lastName = stu_last_name.getText();
         String username = stu_username.getText();
-        //String password = stu_password.getText();
-        String major = stu_first_name.getText();
+        // String password = stu_password.getText();
+        String major = stu_major.getText();
         String classification = stu_classification.getText();
-        
-        if(facade.registerStudent(username, firstName, lastName, "Student", major, classification)) {
+
+        if (facade.registerStudent(username, firstName, lastName, "Student", major, classification)) {
             App.setRoot("studentlanding");
-        } else{
+        } else {
             stu_lbl_error.setText("Invalid");
         }
-     }
+    }
 
-     @FXML
-     void btnAdvSignup(MouseEvent event) throws IOException {
+    @FXML
+    void btnAdvSignup(MouseEvent event) throws IOException {
         String firstName = adv_first_name.getText();
         String lastName = adv_last_name.getText();
         String username = adv_username.getText();
         String department = adv_department.getText();
-        
-        if(facade.registerAdvisor(username, firstName, lastName, "Advisor", department)) {
+
+        if (facade.registerAdvisor(username, firstName, lastName, "Advisor", department)) {
             App.setRoot("advisorlanding");
-        } else{
+        } else {
             adv_lbl_error.setText("Invalid");
         }
-     }
+    }
 
-     
- 
- }
- // test
- 
+}
+// test
